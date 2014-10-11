a555875d3ae6b72a67ddc51d6633a3706b4e5bdb
kevupton <k_upton@live.com>
2014-10-11 20:35:56 +1000

    created pathing

---
+++ Assets/Scripts/PathCreator.cs
@@ -0,0 +1,335 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class PathCreator : MonoBehaviour {
+	public float nodeSize = 1.0f;
+	private Vector2 Size = new Vector2(1.0f, 1.0f);
+	public bool diagonalOn = false;
+	public Vector2 gCost = new Vector2 (10, 14);
+
+	private List<Node> levelNodes = new List<Node> ();
+	private Vector2 startPos;
+	private Vector2 Count;
+
+
+	public Vector2 sw = Vector2.zero;
+	public Vector2 ne = Vector2.zero;
+
+	public class Node {
+		private Vector2 location;
+		public int G = 0;
+		public int H = 0;
+		private int key = 0;
+		private bool isBoundary = false;
+		private Vector2 size;
+		private Node parent = null;
+		private NodeList list;
+
+		public Node(Vector2 loc, int k) {
+			location = loc;
+			key = k;
+		}
+
+		public Node(Vector2 loc, bool ic, int g, int h, int k, Node p) {
+			location = loc;
+			isBoundary = ic;
+			G = g;
+			H = h;
+			key = k;
+			parent = p;
+		}
+
+		public void SetNodeList(NodeList l) {
+			list = l;
+		}
+
+		public NodeList GetNodeList() {
+			return list;
+		}
+
+		public void SetBoundary(bool x) {
+			isBoundary = x;
+		}
+
+		public bool IsBoundary() {
+			return isBoundary;
+		}
+
+		public Node Clone() {
+			return new Node (location, isBoundary, G, H, key, parent);
+		}
+
+		public int GetKey() {
+			return key;
+		}
+
+		public Node GetParent() {
+			return parent;
+		}
+
+		public void SetParent(Node p) {
+			parent = p;
+		}
+
+		public void RemoveParent() {
+			parent = null;
+		}
+
+		public Vector2 GetLocation() {
+			return location;
+		}
+
+		public bool HasParent() {
+			if (parent != null) return true;
+			else return false;
+		}
+
+		public int F {
+			get {
+				return G + H;
+			}
+		}
+		public void SetSize(Vector2 sz) {
+			size = sz;
+		}
+
+		public Vector2 GetSize() {
+			return size;
+		}
+	}
+
+	public class NodeList: IEnumerable {
+		private List<Node> nodeList = new List<Node> ();
+		private Vector2 count;
+		private Vector2 start;
+		private Vector2 size;
+		private Node destNode;
+		private Node startNode;
+		private bool diagonal;
+		private Vector2 cost;
+		private int position = 0;
+
+		private class Enumerator: IEnumerator {
+			private List<Node> nodeList;
+			private int position = -1;
+
+			public Enumerator(List<Node> list) {
+				nodeList = list;
+			}
+
+			private IEnumerator getEnumerator()
+			{
+				return (IEnumerator)this;
+			}
+
+			public void Reset() {
+				position = -1;
+			}
+			
+			public bool MoveNext() {
+				position++;
+				return (position < nodeList.Count);
+			}
+			
+			public object Current {
+				get 
+				{ 
+					try 
+					{
+						return nodeList[position];
+					}
+					
+					catch (IndexOutOfRangeException)
+					{
+						throw new InvalidOperationException();
+					}
+				}
+			}
+		}
+
+		public IEnumerator GetEnumerator()
+		{
+			return new Enumerator(nodeList);
+		}
+
+		public NodeList(Vector2 c, Vector2 st, Vector2 sz, Vector2 cst, bool dia = false){
+			count = c;
+			start = st;
+			size = sz;
+			diagonal = dia;
+			cost = cst;
+		}
+
+		public bool IsDiagonal() {
+			return diagonal;
+		}
+
+		public void SetStartNode(Transform s) {
+			startNode = GetClosestNode(s);
+		}
+
+		public void SetDestNode(Transform d) {
+			destNode = GetClosestNode(d);
+		}
+
+		public Node GetStartNode() {
+			return startNode;
+		}
+
+		public Node GetEndNode() {
+			return destNode;
+		}
+
+		public Node[] GetChildren(Node node) {
+			Vector2 pos = GetNodePos (node);
+			int x = (int) pos.x;
+			int y = (int) pos.y;
+
+			Node[] children = new Node[0];
+
+			for (int s = (x > 0)? x - 1: 0; s <= ((x >= (count.x - 1))? x: x + 1); s++) {
+				for (int v = (y > 0)? y - 1: 0; v <= ((y >= (count.y -1))? y: y + 1); v++) {
+					if (diagonal || (!diagonal && (x == s || y == v))) {
+						int key = (int) (count.x * v + s);
+						
+						Node n = nodeList[key];
+						
+						if (!n.HasParent() && n!= startNode && !n.IsBoundary()) {
+							n.SetParent(node);
+							SetG (n);
+							SetH (n);
+							Array.Resize(ref children, children.Length + 1);
+							children[children.Length - 1] = n;
+						}
+					}
+				}
+			}
+			
+			return children;
+		}
+		
+		public Vector2 GetNodePos(Node node) {
+			int key = node.GetKey ();
+			
+			int x = (int) (key % count.x);
+			int y = (int) Math.Floor (key / count.x);
+			
+			return new Vector2 (x, y);
+		}
+
+		public Node GetNodeAtPos(Vector2 v) {
+			int x = (int)v.x;
+			int y = (int)v.y;
+			int key = (int) (count.x * y + x);
+			
+			return nodeList[key];
+		}
+		
+		Node GetClosestNode(Transform obj) {
+			int x = (int) Math.Round((obj.position.x - start.x) / size.x);
+			int y = (int) Math.Round((start.y - obj.position.y) / size.y);
+			return GetNodeAtPos (new Vector2 (x, y));
+		}
+
+		void SetH(Node node) {
+			Vector2 nodePos = GetNodePos (node);
+			Vector2 destPos = GetNodePos (destNode);
+
+			node.H = (int) (Math.Abs (destPos.y - nodePos.y) + Math.Abs (destPos.x - nodePos.x));
+		}
+		
+		void SetG(Node node) {
+			Vector2 nodePos = GetNodePos (node);
+			Vector2 parPos = GetNodePos (node.GetParent());
+			int c = 0;
+			if (parPos.x == nodePos.x || parPos.y == nodePos.y) {
+				c = (int) cost.x;
+			} else {
+				c = (int) cost.y;
+			}
+			if (node.HasParent ()) {
+				c += node.GetParent().G;
+			}
+			node.G = c;
+		}
+
+		public void Add(Node node) {
+			Node copy = node.Clone ();
+			copy.SetSize (size);
+			copy.SetNodeList (this);
+			nodeList.Add (copy);
+		}
+
+		public int Length {
+			get {
+				return nodeList.Count;
+			}
+		}
+
+		public Vector2 GetSize() {
+			return size;
+		}
+
+		public Vector2 GetDimensions() {
+			return count;
+		}
+	}
+
+	// Use this for initialization
+	void Awake () {
+		CreateNodes (sw, ne);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public NodeList GetNodeList(Transform startObj, Transform destObj) {
+		NodeList list = new NodeList (Count, startPos, Size, gCost, diagonalOn);
+		foreach (Node n in levelNodes) {
+			list.Add(n);
+		}
+		list.SetDestNode (destObj);
+		list.SetStartNode (startObj);
+
+		return list;
+	}
+
+	public void CreateNodes(Vector2 sw, Vector2 ne) {
+		Count = new Vector2(
+			(float) Math.Floor(Math.Abs (((sw.x - ne.x) / nodeSize))),
+ 			(float) Math.Floor(Math.Abs (((sw.y - ne.y) / nodeSize)))
+			);
+
+		Size = new Vector2 (Math.Abs (sw.x - ne.x)/Count.x, Math.Abs (sw.y - ne.y)/Count.y);
+		startPos = new Vector2(Size.x / 2 + sw.x, ne.y - Size.y / 2);
+		int z = 0;
+		for (int y = 0; y < Count.y; y++) {
+			for (int x = 0; x < Count.x; x++) {
+				Vector2 curPos = new Vector2(Size.x * x + startPos.x, startPos.y - Size.y * y);
+				Node node = new Node(curPos, z);
+				node.SetSize(Size);
+				node.SetBoundary(IsWall(node));
+				levelNodes.Add(node);
+				z++;
+			}
+		}
+	}
+	
+	bool IsWall(Node node) {
+		Vector2 topLeft = new Vector2 (node.GetLocation().x - node.GetSize().x/2, node.GetLocation().y + node.GetSize().y/2);
+		Vector2 botRight = new Vector2 (node.GetLocation().x + node.GetSize().x/2, node.GetLocation().y - node.GetSize().y/2);
+		Collider2D[] objects = Physics2D.OverlapAreaAll(topLeft, botRight);
+		foreach (Collider2D obj in objects) {
+			if (obj.tag == "Wall") {
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+
---
+++ Assets/Scripts/PathFinder.cs
@@ -0,0 +1,195 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class PathFinder : MonoBehaviour {
+	public Transform target;
+	public bool pathFound = false;
+
+	public bool drawButtons = true;
+
+	private PathCreator.Node startNode;
+	private PathCreator.Node endNode;
+	private PathCreator.NodeList nodeList;
+	private List<PathCreator.Node> OpenNodes = new List<PathCreator.Node> ();
+	private List<PathCreator.Node> ClosedNodes = new List<PathCreator.Node> ();
+	private PathCreator pathCreator;
+
+	private PathCreator.Node[] pathway = new PathCreator.Node[0];
+
+	public float refreshTime = 1.0f;
+	private float cooldown = 0.0f;
+
+	void Awake() {
+		pathCreator = GameObject.FindWithTag ("PathCreator").GetComponent<PathCreator> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		nodeList = pathCreator.GetNodeList (transform, target);
+		startNode = nodeList.GetStartNode ();
+		endNode = nodeList.GetEndNode ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!pathFound && Input.GetMouseButton(1)) {
+			GetPath ();
+		}
+	}
+
+	void GetPath() {
+		if (!pathFound) {
+			//pathFound = true;
+			if (OpenNodes.Count == 0) {
+				if (ClosedNodes.Count == 0) {
+					GetChildren(startNode);
+				} else {
+					pathFound = true;
+				}
+			}
+			if (ClosedNodes.Count == 0) {
+				ClosedNodes.Add(startNode);
+			}
+			PathCreator.Node smallest = null;
+
+			foreach (PathCreator.Node n in OpenNodes) {
+				if (smallest == endNode) {
+					pathFound = true;
+					CreatePathway(smallest);
+				} else {
+					if (smallest == null) {
+						smallest = n;
+					} else {
+						if (n.F < smallest.F) {
+							smallest = n;
+						}
+					}
+				}
+			}
+			if (smallest != null && !pathFound) {
+				OpenNodes.Remove(smallest);
+				ClosedNodes.Add(smallest);
+				GetChildren(smallest);
+			}
+		}
+	}
+
+	void OnGUI() {
+		if (drawButtons) {
+			Vector2 pos;
+			Vector3 sizea = nodeList.GetSize ();
+			Vector3 sizeb = Camera.main.WorldToScreenPoint(sizea);
+			Vector3 center = Camera.main.WorldToScreenPoint(Vector3.zero);
+			sizeb = sizeb - center;
+
+			GUI.backgroundColor = Color.green;
+			foreach (PathCreator.Node n in OpenNodes) {
+				if (n == endNode) GUI.backgroundColor = Color.yellow;
+				pos = Camera.main.WorldToScreenPoint(new Vector3(n.GetLocation().x - sizea.x/2, -n.GetLocation().y - sizea.y/2));
+				if (GUI.Button (new Rect (pos.x, pos.y, sizeb.x, sizeb.y), "")) {
+					Debug.Log (n.F + " " + n.H + " " + n.G);
+				}
+				if (n == endNode) GUI.backgroundColor = Color.green;
+			}
+
+			GUI.backgroundColor = Color.red;
+			foreach (PathCreator.Node n in ClosedNodes) {
+				if (Array.IndexOf(pathway, n) != -1) GUI.backgroundColor = Color.grey;
+				if (n == startNode) GUI.backgroundColor = Color.blue;
+				pos = Camera.main.WorldToScreenPoint(new Vector3(n.GetLocation().x - sizea.x/2, -n.GetLocation().y - sizea.y/2));
+				//Debug.Log (new Vector3(n.GetLocation().x - sizea.x/2, n.GetLocation().y + sizea.y/2) + " " + pos);
+				if (GUI.Button (new Rect (pos.x, pos.y, sizeb.x, sizeb.y), "")) {
+					Debug.Log (n.F + " " + n.H + " " + n.G);
+				}
+				if (n == startNode || Array.IndexOf(pathway, n) != -1) GUI.backgroundColor = Color.red;
+			}
+		}
+	}
+
+	void GetChildren(PathCreator.Node rent) {
+		foreach (PathCreator.Node n in nodeList.GetChildren(rent)) {
+			OpenNodes.Add(n);
+		}
+	}
+
+	void CreatePathway(PathCreator.Node node) {
+		PathCreator.Node prevNode, prevPrevNode;
+		bool cont = true;
+		while(cont) {
+			Array.Resize(ref pathway, pathway.Length + 1);
+			pathway[pathway.Length - 1] = node;
+			prevNode = node;
+			if (node.HasParent()) {
+				node = node.GetParent();
+			} else {
+				cont =false;
+			}
+		}
+		Array.Reverse (pathway);
+		if (node.GetNodeList().IsDiagonal()) {
+			ShortenPathway ();
+		}
+	}
+
+	void ShortenPathway() {
+		int xp, yp, curp;
+		List<int> optimized = new List<int> ();
+		List<int> keys = new List<int> ();
+		bool cont = true;
+		bool isWall = false;
+
+		PathCreator.Node nodeI, nodeX;
+		Vector2 iPos, xPos, newPos;
+		for (int i = 0; i < pathway.Length; i++) {
+			if (!optimized.Contains(i)) {
+				nodeI = pathway[i];
+				iPos = nodeI.GetNodeList().GetNodePos(nodeI);
+				cont = true;
+				isWall = false;
+				for (int x = i + 1; x < pathway.Length; x++) {
+					if (cont) {
+						keys.Clear();
+						nodeX = pathway[x];
+						xPos = nodeI.GetNodeList().GetNodePos(nodeX);
+						if (((x - i) == 1) && (xPos.x == iPos.x || xPos.y == iPos.y)) {
+							cont = false;
+						} else if (xPos.x == iPos.x || xPos.y == iPos.y) {
+							for (int z = i + 1; z < x; z++) {
+								keys.Add(z);
+								if (xPos.x == iPos.x) {
+									yp = (int) ((iPos.y < xPos.y)? z - i + iPos.y: iPos.y - (z - i));
+									newPos = new Vector2(iPos.x, yp);
+								} else {
+									xp = (int) ((iPos.x < xPos.x)? z - i + iPos.x: iPos.x - (z - i));
+									newPos = new Vector2(xp, iPos.y);
+								}
+								if (nodeI.GetNodeList().GetNodeAtPos(newPos).IsBoundary()) {
+									isWall = true;
+									cont = false;
+								}
+							}
+							if (!isWall) {
+								curp = 0;
+								foreach (int k in keys) {
+									curp++;
+									if (xPos.x == iPos.x) {
+										yp = (int) ((iPos.y < xPos.y)? curp + iPos.y: iPos.y - curp);
+										newPos = new Vector2(iPos.x, yp);
+									} else {
+										xp = (int) ((iPos.x < xPos.x)? curp + iPos.x: iPos.x - curp);
+										newPos = new Vector2(xp, iPos.y);
+									}
+									pathway[k] = nodeI.GetNodeList().GetNodeAtPos(newPos);
+									optimized.Add(k);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+}
